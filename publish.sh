29137a83e054d7f28397aa3b456b08d03a02a70f
Anton Kurako <goodforgod.dev@gmail.com>
2022-01-26 02:30:45 +0300

    GraalVM updated to 21.3.1 Gradle updated to 7.3.3 Maven updated to 3.8.4

---
+++ publish.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+GRAALVM_VERSION="21.3.1"
+JAVA_11_VERSION="java11"
+JAVA_17_VERSION="java17"
+PLATFORM_AMD="amd64"
+PLATFORM_ARM="arm64v8"
+
+# BUILD
+## JAVA 11
+docker build -t goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_11_VERSION} ./${JAVA_11_VERSION}/${PLATFORM_AMD}
+docker build -t goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_11_VERSION}-${PLATFORM_AMD} ./${JAVA_11_VERSION}/${PLATFORM_AMD}
+docker build -t goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_11_VERSION}-${PLATFORM_ARM} ./${JAVA_11_VERSION}/${PLATFORM_ARM}
+
+## JAVA 17
+docker build -t goodforgod/amazonlinux-graalvm:latest ./${JAVA_17_VERSION}/${PLATFORM_AMD}
+docker build -t goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION} ./${JAVA_17_VERSION}/${PLATFORM_AMD}
+docker build -t goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_17_VERSION} ./${JAVA_17_VERSION}/${PLATFORM_AMD}
+docker build -t goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_17_VERSION}-${PLATFORM_AMD} ./${JAVA_17_VERSION}/${PLATFORM_AMD}
+docker build -t goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_17_VERSION}-${PLATFORM_ARM} ./${JAVA_17_VERSION}/${PLATFORM_ARM}
+
+
+# PUSH
+## JAVA 11
+docker push goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_11_VERSION}-${PLATFORM_ARM}
+docker push goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_11_VERSION}-${PLATFORM_AMD}
+docker push goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_11_VERSION}
+
+## JAVA 17
+docker push goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_17_VERSION}-${PLATFORM_ARM}
+docker push goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_17_VERSION}-${PLATFORM_AMD}
+docker push goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}-${JAVA_17_VERSION}
+docker push goodforgod/amazonlinux-graalvm:${GRAALVM_VERSION}
+docker push goodforgod/amazonlinux-graalvm:latest
